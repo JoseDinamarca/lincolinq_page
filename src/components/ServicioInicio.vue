7350341a533261ddaf6fe44e95b70290321f82a3
raulantonio <raulantoniomb1994@gmail.com>
2023-04-27 17:26:56 -0400

    Modificación de serviciosinicio

--- src/components/ServicioInicio.vue
+++ src/components/ServicioInicio.vue
@@ -1,10 +1,14 @@
 <template>
   <section class="section-service">
-    <div class="container mb-5 mb-md-6 mb-xl-10 ">
+    <div class="container mb-5 mb-md-6 mb-xl-10">
       <div class="row justify-content-md-center">
         <div class="col-12 col-md-10 col-lg-8 col-xl-8 text-center">
-          <h5 class="card-title service-subheading-inicio" >Servicios</h5>
-          <h2 class="card-title card-title-heading center service-heading-inicio ">Ayudamos a las empresas a transformar su negocio y su tecnología</h2>
+          <h5 class="card-title service-subheading-inicio">Servicios</h5>
+          <h2
+            class="card-title card-title-heading center service-heading-inicio"
+          >
+            Ayudamos a las empresas a transformar su negocio y su tecnología
+          </h2>
         </div>
       </div>
     </div>
@@ -12,84 +16,110 @@
       <div class="col-12 col-sm-6 col-xl-4 column">
         <div class="card-service">
           <div class="icon-wrapper">
-            <svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" fill="currentColor" class="bi bi-mouse"
-              viewBox="0 0 16 17">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="50"
+              height="50"
+              fill="currentColor"
+              class="bi bi-mouse"
+              viewBox="0 0 16 17"
+            >
               <path
-                d="M8 3a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 3zm4 8a4 4 0 0 1-8 0V5a4 4 0 1 1 8 0v6zM8 0a5 5 0 0 0-5 5v6a5 5 0 0 0 10 0V5a5 5 0 0 0-5-5z" />
+                d="M8 3a.5.5 0 0 1 .5.5v2a.5.5 0 0 1-1 0v-2A.5.5 0 0 1 8 3zm4 8a4 4 0 0 1-8 0V5a4 4 0 1 1 8 0v6zM8 0a5 5 0 0 0-5 5v6a5 5 0 0 0 10 0V5a5 5 0 0 0-5-5z"
+              />
             </svg>
           </div>
           <h3 class="service-heading">Soporte 24/7</h3>
           <p class="service-p">
-            Lorem ipsum dolor sit cuchuflí barquillo bacán jote gamba listeilor po cahuín, luca melón con vino.
+            Lorem ipsum dolor sit cuchuflí barquillo bacán jote gamba listeilor
+            po cahuín, luca melón con vino.
           </p>
         </div>
       </div>
       <div class="col-12 col-sm-6 col-xl-4 column">
         <div class="card-service">
           <div class="icon-wrapper">
-            <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" class="bi bi-chat-text "
-              viewBox="0 0 15 17">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="45"
+              height="45"
+              fill="currentColor"
+              class="bi bi-chat-text"
+              viewBox="0 0 15 17"
+            >
               <path
-                d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z" />
+                d="M2.678 11.894a1 1 0 0 1 .287.801 10.97 10.97 0 0 1-.398 2c1.395-.323 2.247-.697 2.634-.893a1 1 0 0 1 .71-.074A8.06 8.06 0 0 0 8 14c3.996 0 7-2.807 7-6 0-3.192-3.004-6-7-6S1 4.808 1 8c0 1.468.617 2.83 1.678 3.894zm-.493 3.905a21.682 21.682 0 0 1-.713.129c-.2.032-.352-.176-.273-.362a9.68 9.68 0 0 0 .244-.637l.003-.01c.248-.72.45-1.548.524-2.319C.743 11.37 0 9.76 0 8c0-3.866 3.582-7 8-7s8 3.134 8 7-3.582 7-8 7a9.06 9.06 0 0 1-2.347-.306c-.52.263-1.639.742-3.468 1.105z"
+              />
               <path
-                d="M4 5.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zM4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8zm0 2.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5z" />
+                d="M4 5.5a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7a.5.5 0 0 1-.5-.5zM4 8a.5.5 0 0 1 .5-.5h7a.5.5 0 0 1 0 1h-7A.5.5 0 0 1 4 8zm0 2.5a.5.5 0 0 1 .5-.5h4a.5.5 0 0 1 0 1h-4a.5.5 0 0 1-.5-.5z"
+              />
             </svg>
           </div>
           <h3 class="service-heading">Chat online</h3>
           <p class="service-p">
-            Lorem ipsum dolor sit cuchuflí barquillo bacán jote gamba listeilor po cahuín, luca melón con vino.
+            Lorem ipsum dolor sit cuchuflí barquillo bacán jote gamba listeilor
+            po cahuín, luca melón con vino.
           </p>
         </div>
       </div>
       <div class="col-12 col-sm-6 col-xl-4 column">
         <div class="card-service">
           <div class="icon-wrapper">
-            <svg xmlns="http://www.w3.org/2000/svg" width="45" height="45" fill="currentColor" class="bi bi-heart-pulse  "
-              viewBox="0 -1 16 17">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              width="45"
+              height="45"
+              fill="currentColor"
+              class="bi bi-heart-pulse"
+              viewBox="0 -1 16 17"
+            >
               <path
-                d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z" />
+                d="M8 4.754a3.246 3.246 0 1 0 0 6.492 3.246 3.246 0 0 0 0-6.492zM5.754 8a2.246 2.246 0 1 1 4.492 0 2.246 2.246 0 0 1-4.492 0z"
+              />
               <path
-                d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z" />
+                d="M9.796 1.343c-.527-1.79-3.065-1.79-3.592 0l-.094.319a.873.873 0 0 1-1.255.52l-.292-.16c-1.64-.892-3.433.902-2.54 2.541l.159.292a.873.873 0 0 1-.52 1.255l-.319.094c-1.79.527-1.79 3.065 0 3.592l.319.094a.873.873 0 0 1 .52 1.255l-.16.292c-.892 1.64.901 3.434 2.541 2.54l.292-.159a.873.873 0 0 1 1.255.52l.094.319c.527 1.79 3.065 1.79 3.592 0l.094-.319a.873.873 0 0 1 1.255-.52l.292.16c1.64.893 3.434-.902 2.54-2.541l-.159-.292a.873.873 0 0 1 .52-1.255l.319-.094c1.79-.527 1.79-3.065 0-3.592l-.319-.094a.873.873 0 0 1-.52-1.255l.16-.292c.893-1.64-.902-3.433-2.541-2.54l-.292.159a.873.873 0 0 1-1.255-.52l-.094-.319zm-2.633.283c.246-.835 1.428-.835 1.674 0l.094.319a1.873 1.873 0 0 0 2.693 1.115l.291-.16c.764-.415 1.6.42 1.184 1.185l-.159.292a1.873 1.873 0 0 0 1.116 2.692l.318.094c.835.246.835 1.428 0 1.674l-.319.094a1.873 1.873 0 0 0-1.115 2.693l.16.291c.415.764-.42 1.6-1.185 1.184l-.291-.159a1.873 1.873 0 0 0-2.693 1.116l-.094.318c-.246.835-1.428.835-1.674 0l-.094-.319a1.873 1.873 0 0 0-2.692-1.115l-.292.16c-.764.415-1.6-.42-1.184-1.185l.159-.291A1.873 1.873 0 0 0 1.945 8.93l-.319-.094c-.835-.246-.835-1.428 0-1.674l.319-.094A1.873 1.873 0 0 0 3.06 4.377l-.16-.292c-.415-.764.42-1.6 1.185-1.184l.292.159a1.873 1.873 0 0 0 2.692-1.115l.094-.319z"
+              />
             </svg>
           </div>
           <h3 class="service-heading">Desarrollo a medida</h3>
           <p class="service-p">
-            Lorem ipsum dolor sit cuchuflí barquillo bacán jote gamba listeilor po cahuín, luca melón con vino.
+            Lorem ipsum dolor sit cuchuflí barquillo bacán jote gamba listeilor
+            po cahuín, luca melón con vino.
           </p>
         </div>
-
       </div>
-      <div class="container mb-5 mb-md-6 mb-xl-10 ">
-      <div class="row justify-content-md-center">
-        <div class="col-12 col-md-10 col-lg-8 col-xl-8 text-center">
-          <button class="btn service-button-inicio">Más Servicios <i class="bi bi-arrow-right"></i></button>
+      <div class="container mb-5 mb-md-6 mb-xl-10">
+        <div class="row justify-content-md-center">
+          <div class="col-12 col-md-10 col-lg-8 col-xl-8 text-center">
+            <button class="btn service-button-inicio">
+              Más Servicios <i class="bi bi-arrow-right"></i>
+            </button>
+          </div>
         </div>
-      </div>
       </div>
     </div>
   </section>
 </template>
 
 <style>
-
 .service-button-inicio {
-    left: 0 !important;
-    height: 50px !important;
-    float: center !important;
-    margin-top: 5% !important;
-    background-color: #2c7bfe !important;
-    color: white !important;
-  }
+  left: 0 !important;
+  height: 50px !important;
+  float: center !important;
+  margin-top: 5% !important;
+  background-color: #2c7bfe !important;
+  color: white !important;
+}
 
-  .service-button-inicio:hover {
-    left: 0 !important;
-    height: 50px !important;
-    float: center !important;
-    margin-top: 5% !important;
-    background: linear-gradient(#2c7bfe 20%, #176cf5 50%) !important;
-    transition: 1s;
-    color: white !important;
-  }
+.service-button-inicio:hover {
+  left: 0 !important;
+  height: 50px !important;
+  float: center !important;
+  margin-top: 5% !important;
+  background: linear-gradient(#2c7bfe 20%, #176cf5 50%) !important;
+  transition: 1s;
+  color: white !important;
+}
 
 .section-service {
   padding: 4.5rem 0;
@@ -101,18 +131,18 @@
 }
 
 .service-heading-inicio {
-  padding-top:10px;
+  padding-top: 10px;
   font-family: sourcesanspro !important;
 }
 
-.service-subheading-inicio{
+.service-subheading-inicio {
   transform: translate(0px);
-    font-size: 16px;
-    margin-bottom: 0.3125rem;
-    text-transform: uppercase;
-    color: #004ff0 !important;
-    font-family: sourcesanspro;
-text-align: center;
+  font-size: 16px;
+  margin-bottom: 0.3125rem;
+  text-transform: uppercase;
+  color: #004ff0 !important;
+  font-family: sourcesanspro;
+  text-align: center;
 }
 
 .service-p {
@@ -126,7 +156,6 @@ text-align: center;
   text-align: center;
 }
 
-
 .card-service {
   width: 100%;
   height: 100%;
@@ -136,7 +165,7 @@ text-align: center;
   background-position: 0 2.5%;
   border-radius: 10px;
   border-width: 1px;
-  border-color: #D4D4E1;
+  border-color: #d4d4e1;
   border-style: solid;
   cursor: pointer;
   transition: 0.5s;
@@ -164,7 +193,6 @@ text-align: center;
 .card-service:hover .icon-wrapper {
   background-color: #ffffff;
   color: #2c7bfe;
-
 }
 
 .card-service:hover h3 {
