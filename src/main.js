cc45cc6e4ae25bf941e26eb172cb45f3d130001a
Luiscaroca <kako.ljca@gmail.com>
2023-04-28 17:33:01 -0400

    Eliminadas dependencias sin utilizar, agregadas dependencias necesarias para envio de correos, agregado archivo server.js para envio de correos

--- src/main.js
+++ src/main.js
@@ -4,23 +4,8 @@ import router from "./router";
 import "bootstrap/dist/css/bootstrap.css";
 import "bootstrap-icons/font/bootstrap-icons.css";
 
-import { BootstrapIconsPlugin } from 'bootstrap-icons-vue';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome';
+import { BootstrapIconsPlugin } from "bootstrap-icons-vue";
 
-import { faSquareFacebook } from '@fortawesome/free-brands-svg-icons';
-import { faInstagram } from '@fortawesome/free-brands-svg-icons';
-import { faWhatsapp } from "@fortawesome/free-brands-svg-icons";
-import { faEnvelope } from "@fortawesome/free-regular-svg-icons";
-import { faCopyright } from "@fortawesome/free-regular-svg-icons";
-
-library.add(faSquareFacebook, faInstagram, faEnvelope, faWhatsapp, faCopyright)
-
-createApp(App)
-.use(router)
-.use(BootstrapIconsPlugin)
-.component('font-awesome-icon', FontAwesomeIcon)
-.mount("#app");
+createApp(App).use(router).use(BootstrapIconsPlugin).mount("#app");
 
 import "bootstrap/dist/js/bootstrap.js";
-
